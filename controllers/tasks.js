978cf99a406645d1de99c7ebb5abe0ece32aa414
Inesh-Reddy <ineshreddy007@gmail.com>
2024-12-17 17:52:04 +0530

    Implemented getAlTasks and getSingleTasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const Task = require('../models/tasks');
 
-const getAllTasks = (req, res)=>{
-  res.send('all items from the file')
+const getAllTasks = async (req, res)=>{
+  try{
+    const tasks = await Task.find({});
+    res.status(200).json({
+      tasks
+    })
+  }catch(err){
+      res.status(500).json({
+        "msg": err.message,
+      })
+  }
 }
 const createTasks = async (req, res)=>{
   try{
@@ -12,8 +21,33 @@ const createTasks = async (req, res)=>{
     res.status(500).json({"msg":err.message})
   }
 }
-const getTasks = (req, res)=>{
-  res.send('get single task')
+const getTasks =  async (req, res)=>{
+  // try{
+  //   const task = await Task.findById(`${req.body._id}`);
+  //   if (!task) {
+  //       return res.status(404).json({
+  //           msg: "Task not found"
+  //       });
+  //   }
+  //   res.status(200).json({ msg: task });
+  // }
+  // 
+  // 
+  try{
+    const task = await Task.findOne({name:`${req.body.name}`});
+    console.log(task);
+    if (!task) {
+        return res.status(404).json({
+            msg: "Task not found"
+        });
+    }
+    res.status(200).json({ msg: task });
+  }catch(err){
+    res.status(500).json({
+      msg: err.message
+    })
+  }
+  
 }
 const updateTasks = (req, res)=>{
   res.send('update task')
